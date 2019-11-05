4b43c9c74a886a50e6a5e1cd9dad899cf80c98e1
Hugh Caluscusin (Dev) <hugh@melodiccrypter.com>
2019-11-05 17:09:17 +0800

    added graphql config, imported prisma

--- src/prisma.js
+++ src/prisma.js
@@ -1,6 +1,10 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-    typeDefs: '',
+    typeDefs: '../prisma/generated/prisma-client/prisma.graphql',
     endpoint: 'localhost:4466'
+});
+
+prisma.query.users(null, '{ id name email }').then(data => {
+    console.log(data);
 });
