df44eb21f3bd909dfeed256efad5d9b1ec5e2352
Hugh Caluscusin (Dev) <hugh@melodiccrypter.com>
2019-11-07 11:53:56 +0800

    added new prisma endpoint

--- src/prisma.js
+++ src/prisma.js
@@ -1,10 +1,50 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-    typeDefs: '../prisma/generated/prisma-client/prisma.graphql',
-    endpoint: 'localhost:4466'
+    typeDefs: 'prisma/generated/prisma-client/prisma.graphql',
+    endpoint: 'http://localhost:4466'
 });
 
-prisma.query.users(null, '{ id name email }').then(data => {
-    console.log(data);
-});
+// prisma.query.users(null, '{ id name }')
+//     .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//     })
+//     .catch(e => console.log(e));
+
+// const createPostForUser = async (authorId, data) => {
+//   const userExists  = await prisma.exists.User({ id: authorId });
+//
+//   if (!userExists) {
+//       throw new Error('User not found');
+//   }
+//
+//   const post =  await prisma.mutation.createPost({
+//       data: {
+//           ...data,
+//           author: {
+//               connect: {
+//                   id: authorId
+//               }
+//           }
+//       }
+//   }, '{ author { id name email posts { id title published } } }');
+//
+//   return post.author;
+// };
+
+// const updatePostForUser = async (postId, data) => {
+//   const postExists = await prisma.exists.Post({ id: postId });
+//
+//   if (!postExists) {
+//       throw new Error('Post not found');
+//   }
+//
+//   const post = await prisma.mutation.updatePost({
+//       where: {
+//           id: postId
+//       },
+//       data
+//   }, '{  author { id name email posts { id title published } } }');
+//
+//   return post.author;
+// };
